701434292037ac4c7e18cc9dcbe4cee771f68f32
Jeffrey King <jking@hey.com>
2022-07-27 19:01:15 +0300

    rename constant in Avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,12 +4,12 @@ type Props = {
 };
 
 const Avatar = ({ src, isHighlighted }: Props) => {
-  const ringcolor = isHighlighted
+  const ringClassNames = isHighlighted
     ? "ring-4 ring-rose-500"
     : "ring-2 ring-gray-300";
   return (
     <img
-      className={`inline-block h-16 w-16 rounded-full p-1 ring-2 ${ringcolor}`}
+      className={`inline-block h-16 w-16 rounded-full p-1 ring-2 ${ringClassNames}`}
       src={src}
       alt="MIA"
     />
