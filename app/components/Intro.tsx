a15f32146ecbdafa5a14363412680d5793a795f0
Jeffrey King <jking@hey.com>
2022-09-06 10:56:41 -0400

    adjust intro and photo

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -5,36 +5,38 @@ const Intro = () => {
     <div className="m-auto max-w-[1024px]">
       <h2 className="text-6xl">Hakuna Matatu</h2>
       <img className="m-auto mt-8 " src={hakunaMatatu} />
-      <div className="flex">
-        <div className="max-w-[500px]">
-          <p className="text-left text-lg">
-            …but this particular Toyota Prado was responsible for many of my
-            most unforgettable moments during my time in Kenya. At the most
-            basic level, it was the vehicle of transporting our group of
-            researchers from our research base to fieldwork on a daily basis,
-            but it was so much more. Through its windows, we’d see wildlife up
-            close - elephants, giraffes, baboons, hyrax, many within twenty five
-            meters. In its belly, we honed our Swahili, piecing together
-            whatever Swahili we learned into sentences that wouldn’t make sense
-            to anyone who hadn’t sat in this vehicle. Everyday, a new cast of
-            characters would ride with us to our destinations. We’d squeeze into
-            the vehicle haphazardly, with the first passengers squeezing through
-            the trunk of the vehicle to sit in the third row. Each day, a
-            different seating configuration developed, resulting in fresh
-            conversations and dynamics from where each person sat. The following
-            visualization documents where each person sat on each trip, and
-            aggregates the seating to visualize where each person most often
-            sat.
-          </p>
-        </div>
-        <div className="-mt-16 mr-8 bg-white p-4">
+      <div className="flex flex-wrap ">
+        <div className="-mt-16 ml-8 mr-4 bg-white p-2 sm:w-full md:max-w-[45%]">
           <img src="images/prado_inside.jpg" />
-          <video className="mt-4" autoPlay muted loop>
+          <video className="mt-2" autoPlay muted loop>
             <source src="images/prado_buffalo.mov" type="video/mp4" />
           </video>
-          <video className="mt-4" autoPlay muted loop>
+          <video className="mt-2" autoPlay muted loop>
             <source src="images/prado_plant_move.mov" type="video/mp4" />
           </video>
+        </div>
+        <div className=" p-4 text-left text-lg sm:w-full md:max-w-[45%] ">
+          <p className="mt-4 text-lg">
+            …but this particular Toyota Prado was home to many of my most
+            unforgettable memories in Kenya. It was of course the vehicle for
+            transporting us from our research base to fieldwork on a daily
+            basis, but it was also a lens into Kenya’s beautiful landscapes and
+            local culture. Through its windows, we saw wildlife up close -
+            elephants, giraffes, baboons, and hyraxes. During our drives, we
+            honed our Swahili, discussed differences in cultural values, and
+            joked our way through a variety of topics.
+          </p>
+          <p className="mt-4">
+            Everyday, a new cast of characters would jam into the Prado. Those
+            sitting in the third row would squeeze in first by either entering
+            through the trunk or hopping over the second row seats. Only then
+            could the second row folks file into place. With every new seating
+            arrangement, fresh conversations would sprout. The following
+            visualization shows where each person sat on each trip over a nine
+            day period. It also aggregates the frequency each rider sat in a
+            particular space to show where each rider spent most of their time
+            when riding in the Prado.
+          </p>
         </div>
       </div>
     </div>
